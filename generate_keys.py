7dfb99469fb4aa806e6d2544c7f3b42a2f54578c
michele <mbarrant@iu.edu>
2024-12-02 13:29:00 -0500

    gen key file

---
+++ generate_keys.py
@@ -0,0 +1,31 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+
+def generate_keys():
+
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
+
+    public_key = private_key.public_key()
+
+
+    #save keys to file
+    with open("private_key.pem", "wb") as priv_file:
+        priv_file.write(private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption(),
+        ))
+
+    with open("public_key.pem", "wb") as pub_file:
+        pub_file.write(public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo,
+        ))
+
+
+generate_keys()
